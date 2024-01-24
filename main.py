6d317084cbaec1687b88651bc6a6a7014ac03648
fullstackprocrastinator <bengouldingcs@gmail.com>
2024-01-24 17:00:42 +0000

    updated script to take input and provide count

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    word_count = count_words(text)
+    print(text)
+
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+    
+main()
